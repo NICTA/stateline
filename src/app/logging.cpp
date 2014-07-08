418daad6dcddf2d5600c8db78245fdf7b41c55a5
darrnshn <darren.shen@nicta.com.au>
2014-07-08 11:32:53 +1000

    Rename console to logging

---
+++ src/app/logging.cpp
@@ -0,0 +1,40 @@
+//!
+//! Contains the implementation of logging.
+//!
+//! \file app/logging.cpp
+//! \author Lachlan McCalman
+//! \date 2014
+//! \license Affero General Public License version 3 or later
+//! \copyright (c) 2014, NICTA
+//!
+
+#include "app/logging.hpp"
+
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+
+#include <glog/logging.h>
+
+namespace stateline
+{
+  void initLogging(const std::string &appName, int logLevel, bool stdErr,
+      const std::string &directory)
+  {
+    int normLog = logLevel;
+    int vLevel = 0;
+    if (logLevel < 0)
+    {
+      vLevel = -logLevel;
+      normLog = 0;
+    }
+
+    google::InitGoogleLogging(appName.c_str());
+    FLAGS_logtostderr = stdErr;
+    FLAGS_log_dir = directory;
+    FLAGS_minloglevel = normLog;
+    FLAGS_v = vLevel;
+    LOG(INFO) << "Logging initialised with level " << normLog << " and verbosity " << vLevel;
+  }
+
+} // namespace stateline
