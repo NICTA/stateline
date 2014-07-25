2943717216625357b86c134dd258245c6dc0847f
darrnshn <darren.shen@nicta.com.au>
2014-07-25 10:11:43 +1000

    Update server example to reflect changes

--- python/examples/normal/server.py
+++ python/examples/normal/server.py
@@ -9,7 +9,7 @@ import numpy as np
 loc, scale = 0.0, 1.0
 
 # Parameters of the MCMC set up
-nstacks, nchains = 2, 1
+nstacks, nchains = 1, 1
 
 # Create a delegator and send the distribution parameters
 delegator = sl.Delegator(5555, global_spec=(loc, scale))
@@ -19,13 +19,18 @@ initial = np.random.random((nstacks * nchains, 1))
 mc = mcmc.init(delegator, nstacks, nchains, initial)
 
 # Run MCMC
-samples = mcmc.run(mc, samplers.RWM(),
-                   mcmc.until(nsamples=1000),
-                   mcmc.store_as_list())
+samples = mcmc.run(mc, samplers.RWM(np.ones(1,)),
+                   mcmc.thinning(10),
+                   mcmc.until(nsamples=5000))
 
 # Plot the histogram along with the true distribution
-fig, ax = plt.subplots()
-ax.hist(samples, 50, normed=True)
+fig, (ax1, ax2) = plt.subplots(2)
+ax1.hist(samples, 50, normed=True, alpha=0.5)
 
 x = np.linspace(-3.0, 3.0, 100)
-ax.plot(x, stats.norm.pdf(x), alpha=0.5)
+ax1.plot(x, stats.norm.pdf(x), color='r')
+
+# Plot a trace to show mixing behaviour
+ax2.plot(range(samples.shape[0]), samples)
+
+plt.show()
