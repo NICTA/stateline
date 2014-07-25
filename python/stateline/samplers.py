1b77f2205fc162ff67a379f6a2758bfa3d04c854
darrnshn <darren.shen@nicta.com.au>
2014-07-25 10:10:37 +1000

    Bugfix metropolis sampler

--- python/stateline/samplers.py
+++ python/stateline/samplers.py
@@ -21,28 +21,28 @@ class RWM(object):
 
     Attributes
     ----------
-    step_sizes : iterable
-        An iterable containing the standard deviations of the proposal
-        distribution in each dimension. The length of the iterable is the
+    step_sizes : array-like
+        A 1d-array containing the standard deviations of the proposal
+        distribution in each dimension. The length of the array is the
         number of dimensions in the problem.
     """
 
-    def __init__(self, step_sizes=None):
+    def __init__(self, step_sizes):
         """Create a new random-walk Metropolis-Hastings sampler.
 
         Parameters
         ----------
-        step_sizes : iterable, optional
+        step_sizes : array-like
             The standard deviation of the size of each Metropolis step (i.e.
-            of the normal distribution). Defaults to 1.0 for all dimensions.
+            of the normal distribution).
         """
         self.step_sizes = step_sizes
 
     def next_state(self, _, old_state, proposed_state):
         delta_logl = proposed_state.logl - old_state.logl
-        should_accept = self.random.random() < math.exp(- delta_logl)
+        should_accept = np.random.random() < math.exp(delta_logl)
         return proposed_state if should_accept else old_state
 
     def next_job(self, _, state):
-        proposal = mcmc.State(state)
-        return np.random.random((self.dims,)) + state.sample
+        step = np.random.normal(size=self.step_sizes.shape) * self.step_sizes
+        return mcmc.State(state.sample + step)
