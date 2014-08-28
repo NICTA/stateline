8ec5a2604455be484c9ba13e9a0ca511bfb264d7
darrnshn <darren.shen@nicta.com.au>
2014-08-28 11:21:02 +1000

    Add python sampler

--- python/cpp/sampler.hpp
+++ python/cpp/sampler.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+#include "infer/sampler.hpp"
+
+void samplerStep(mcmc::Sampler &sampler, py::list sigmas, py::list betas)
+{
+  sampler.step(lnumpy2veigen(sigmas), list2vector<double>(betas));
+}
+
 void exportSampler()
 {
+  py::class_<mcmc::Sampler, boost::noncopyable>("Sampler",
+      .def("step", &samplerStep)
+      .def("flush", &mcmc::Sampler::flush)
+  ;
 }
