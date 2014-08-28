8b400f779b9469a14d68fb3342405e167ba573ed
darrnshn <darren.shen@nicta.com.au>
2014-08-28 11:23:57 +1000

    WIP python state

--- python/stateline/mcmc.py
+++ python/stateline/mcmc.py
@@ -13,29 +13,69 @@ class State(_sl.State):
     ----------
     sample : NumPy array
         The sample from the target distribution which this state represents.
-        This is a 1d-array whose length corresponds to the dimensionality of the
-        target distribution.
-    logl : float, optional
-        The log likelihood of this sample. Defaults to None.
-    beta : float, optional
+        This is a 1d-array whose length corresponds to the dimensionality of
+        the target distribution.
+    energy : float
+        The negative log likelihood of this sample.
+    sigma : float, optional
+        The step size of the chain when this state was evaluated.
+    beta : float
         The inverse temperature of the chain when this state was evaluated.
-        Defaults to None.
+    accepted : boolean
+        If this is True, then this state was an accepted proposed state.
+        Otherwise, this state is the same as the previous state in the chain
+        because it was rejected.
     """
 
     _BASE = _sl.State
 
-    def __init__(self, sample, logl=None, beta=None):
-        self.sample = sample
-        self.logl = logl
-        self.beta = beta
+    def __init__(self, sample, energy=None, sigma=None, beta=None,
+                 accepted=True):
+        self._BASE.setSample(sample)
+        self._BASE.energy = energy
+        self._BASE.setSigma(sigma)
+        self._BASE.energy = energy
+        self._BASE.beta = beta
+        self._BASE.accepted = accepted
+
+    @property
+    def sample(self):
+        return self._BASE.getSample(self)
+
+    @sample.setter
+    def sample(self, val):
+        self._BASE.setSample(self, val)
+
+    @property
+    def sigma(self):
+        return self._BASE.getSigma(self)
+
+    @sigma.setter
+    def sigma(self, val):
+        self._BASE.setSigma(self, val)
+
+    @property
+    def energy(self):
+        return self._BASE.energy
+
+    @property
+    def beta(self):
+        return self._BASE.beta
+
+    @property
+    def accepted(self):
+        return self._BASE.accepted
 
 
 class Sampler(_sl.Sampler):
+    """Represents a Markov-Chain Monte Carlo sampler."""
+
     _BASE = _sl.Sampler
 
-    def __init__(self):
+
+class SlidingWindowSigmaAdapter(_sl.SlidingWindowSigmaAdapter)
+    """"""
+    _BASE = _sl.SlidingWindowSigmaAdapter
+
+    def __init__(self, chainarray?)
         pass
-
-    def step(self, sigmas, betas):
-        return self._BASE.step(self, sigmas, betas)
-
