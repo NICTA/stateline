774fa1744ec3333e01b1be32f60f13de53e847b2
Lachlan McCalman <lachlan.mccalman@nicta.com.au>
2014-09-08 16:15:05 +1000

    updates to build properly

--- prereqs/buildPrereqs.sh
+++ prereqs/buildPrereqs.sh
@@ -14,7 +14,7 @@ cd src
 wget -c http://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.gz
 [ -d boost_1_55_0 ] || tar -xvf boost_1_55_0.tar.gz
 cd boost_1_55_0
-./bootstrap.sh
+./bootstrap.sh --with-python=python3.3
 ./b2 -j $(nproc) --layout=versioned variant=debug,release threading=multi link=static runtime-link=static  toolset=gcc address-model=64 install --prefix=$PREREQ_DIR
 cd ..
 
