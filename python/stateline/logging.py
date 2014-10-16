f65dd3671754aa1717763391e2b605623d27bd1b
darrnshn <darren.shen@nicta.com.au>
2014-10-16 15:22:19 +1100

    BUGFIX: true no logging

--- python/stateline/logging.py
+++ python/stateline/logging.py
@@ -2,8 +2,12 @@ import _stateline as _sl
 
 
 def initialise(log_level, use_std_err, directory):
+    """Initialise the logging system."""
+
     _sl.init_logging("stateline", log_level, use_std_err, directory)
 
 
-def nolog():
-    initialise(0, False, '.')
+def no_logging():
+    """Initialise the logging system to be silent."""
+
+    initialise(4, True, '.')
