a3357ffa96f8bb02ace45ac88cf2aebd7afb2c1c
darrnshn <darren.shen@nicta.com.au>
2014-10-16 15:17:08 +1100

    10 is optimal

--- src/infer/adaptive.cpp
+++ src/infer/adaptive.cpp
@@ -213,7 +213,7 @@ namespace stateline
     void CovarianceEstimator::update(uint i, const Eigen::VectorXd &sample)
     {
       // 10 is theoretically optimal
-      double n = (double)lengths_[i] + 500;
+      double n = (double)lengths_[i] + 10 * sample.size();
 
       a_[i] = a_[i] * (n / (n + 1)) + (sample * sample.transpose()) / (n + 1);
       u_[i] = u_[i] * (n / (n + 1)) + sample / (n + 1);
