12adcfeb4b693c7417901bb2a0150f4d53de5d33
Lachlan McCalman <lachlan.mccalman@nicta.com.au>
2015-04-10 16:20:54 +1000

    add minion and requester

--- src/comms/minion.cpp
+++ src/comms/minion.cpp
@@ -7,56 +7,31 @@
 //!
 
 #include "comms/minion.hpp"
-
-#include <cstdint>
-
-#include "comms/serial.hpp"
+#include <boost/algorithm/string.hpp>
 
 namespace stateline
 {
   namespace comms
   {
-    Minion::Minion(Worker& w, JobType jobType)
-        : socket_(w.zmqContext(), ZMQ_DEALER),
-          jobTypeString_(detail::serialise<std::uint32_t>(jobType))
+    Minion::Minion(zmq::context_t& context, const std::vector<std::string>& jobTypes)
+        : socket_(context, ZMQ_DEALER, "toWorker")
     {
-      auto socketID = stateline::comms::randomSocketID();
-      stateline::comms::setSocketID(socketID, socket_);
       socket_.connect(WORKER_SOCKET_ADDR.c_str());
+      std::string jobstring = boost::algorithm::join(jobTypes, ":");
+      socket_.send({HELLO,{jobstring}});
     }
 
-    std::string Minion::nextJob()
+    std::pair<std::string, std::string> Minion::nextJob()
     {
-      // Make sure we conform to the Stateline protocol
-      if (firstMessage_)
-      {
-        // TODO: what do we do here?
-        send(socket_, Message(stateline::comms::WORK, { jobTypeString_ }));
-        firstMessage_ = false;
-      }
-
       VLOG(3) << "Minion waiting on next job";
-      stateline::comms::Message r = receive(socket_);
-      requesterAddress_ = r.address;
-      std::string addrString = "Minion received address: ";
-      for (auto a : r.address)
-      {
-        addrString += a + "::";
-      }
-      VLOG(3) << addrString;
-
-      // r.data[0] is job type
-      // r.data[1] is the data
-      return r.data[1];
+      stateline::comms::Message r = socket_.receive();  
+      currentJob_ = r.data[1];
+      return std::make_pair(r.data[0], r.data[2]);
     }
 
     void Minion::submitResult(const std::string& result)
     {
-      Message m(requesterAddress_, stateline::comms::WORK,
-          { jobTypeString_, result, jobTypeString_});
-
-      VLOG(3) << "Minion submitting " << m;
-      send(socket_, m);
+      socket_.send({RESULT,{currentJob_,result}});
     }
 
   } // namespace comms
