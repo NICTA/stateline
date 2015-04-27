d21bfec21d997c1316e482de0eebbabc4805ad3f
darrnshn <darren.shen@nicta.com.au>
2015-04-27 15:42:38 +1000

    Add basic unit tests for router

--- src/comms/tests/router.cpp
+++ src/comms/tests/router.cpp
@@ -1,137 +1,110 @@
 //!
-//! \file comms/testrouter.cpp
+//! \file comms/tests/router.cpp
 //! \author Lachlan McCalman
-//! \date 2014
+//! \author Darren Shen
+//! \date 2015
 //! \license Lesser General Public License version 3 or later
 //! \copyright (c) 2014, NICTA
 //!
 
-#include <chrono>
-#include "gtest/gtest.h"
-#include <thread>
+#include <gtest/gtest.h>
 
-#include "comms/messages.hpp"
-#include "comms/transport.hpp"
 #include "comms/router.hpp"
-
-#include <Eigen/Core>
 
 using namespace stateline;
 using namespace stateline::comms;
 
-TEST(Routing, sendAndReceive)
+TEST(Router, canPollRouterWithNoSockets)
 {
-  zmq::context_t context(1);
-  zmq::socket_t alphaSocket(context, ZMQ_PAIR);
-  std::unique_ptr<zmq::socket_t> alphaSocketRouter(new zmq::socket_t(context, ZMQ_PAIR));
-  alphaSocket.bind("inproc://alpha");
-  alphaSocketRouter->connect("inproc://alpha");
-
-  SocketRouter router;
-
-  router.add_socket(SocketID::ALPHA, alphaSocketRouter);
-
-  Message hello(HELLO);
-  Message problemspec(PROBLEMSPEC);
-
-  send(alphaSocket, hello);
-  auto rec = router.receive(SocketID::ALPHA);
-  ASSERT_EQ(hello, rec);
-  router.send(SocketID::ALPHA, problemspec);
-  auto rec2 = receive(alphaSocket);
-  ASSERT_EQ(problemspec, rec2);
-}
-
-TEST(Routing, polling)
-{
-  zmq::context_t context(1);
-  zmq::socket_t alphaSocket(context, ZMQ_PAIR);
-  std::unique_ptr<zmq::socket_t> alphaSocketRouter(new zmq::socket_t(context, ZMQ_PAIR));
-  alphaSocket.bind("tcp://*:5555");
-  alphaSocketRouter->connect("tcp://localhost:5555");
-
-  SocketRouter router;
-  router.add_socket(SocketID::ALPHA, alphaSocketRouter);
-
-  Message hello(HELLO);
-  bool okHELLO = false;
-
-  auto onRcvHELLO = [&] (const Message&m)
-  { okHELLO = m == hello;};
-  router(SocketID::ALPHA).onRcvHELLO.connect(onRcvHELLO);
-
+  zmq::context_t context{1};
   bool running = true;
-  router.start(10, running); //msPerPoll
-  send(alphaSocket, hello);
-  std::this_thread::sleep_for(std::chrono::milliseconds(50));
-  running = false;
-  ASSERT_TRUE(okHELLO);
-}
 
-TEST(Routing, callbacks)
-{
-  zmq::context_t context(1);
-  zmq::socket_t alphaSocket(context, ZMQ_PAIR);
-  zmq::socket_t betaSocket(context, ZMQ_PAIR);
-  std::unique_ptr<zmq::socket_t> alphaSocketRouter(new zmq::socket_t(context, ZMQ_PAIR));
-  std::unique_ptr<zmq::socket_t> betaSocketRouter(new zmq::socket_t(context, ZMQ_PAIR));
-  alphaSocket.bind("tcp://*:5555");
-  betaSocket.bind("tcp://*:5556");
-  alphaSocketRouter->connect("tcp://localhost:5555");
-  betaSocketRouter->connect("tcp://localhost:5556");
-
-  SocketRouter router;
-
-  router.add_socket(SocketID::ALPHA, alphaSocketRouter);
-  router.add_socket(SocketID::BETA, betaSocketRouter);
-
-  Message hello(HELLO);
-  Message heartbeat(HEARTBEAT);
-  Message problemspec(PROBLEMSPEC);
-  Message jobrequest(JOBREQUEST);
-  Message job(JOB);
-  Message jobswap(JOBSWAP);
-  Message alldone(ALLDONE);
-  Message goodbye(GOODBYE);
-
-  auto fwdToBeta = [&] (const Message&m)
-  { router.send(SocketID::BETA, m);};
-  // Bind functionality to the router
-  router(SocketID::ALPHA).onRcvHELLO.connect(fwdToBeta);
-  router(SocketID::ALPHA).onRcvHEARTBEAT.connect(fwdToBeta);
-  router(SocketID::ALPHA).onRcvPROBLEMSPEC.connect(fwdToBeta);
-  router(SocketID::ALPHA).onRcvJOBREQUEST.connect(fwdToBeta);
-  router(SocketID::ALPHA).onRcvJOB.connect(fwdToBeta);
-  router(SocketID::ALPHA).onRcvJOBSWAP.connect(fwdToBeta);
-  router(SocketID::ALPHA).onRcvALLDONE.connect(fwdToBeta);
-  router(SocketID::ALPHA).onRcvGOODBYE.connect(fwdToBeta);
-
-  bool running = true;
-  router.start(10, running);//ms per poll
-  send(alphaSocket, hello);
-  Message rhello = receive(betaSocket);
-  send(alphaSocket, heartbeat);
-  Message rheartbeat = receive(betaSocket);
-  send(alphaSocket, problemspec);
-  Message rproblemspec = receive(betaSocket);
-  send(alphaSocket, jobrequest);
-  Message rjobrequest = receive(betaSocket);
-  send(alphaSocket, job);
-  Message rjob = receive(betaSocket);
-  send(alphaSocket, jobswap);
-  Message rjobswap = receive(betaSocket);
-  send(alphaSocket, alldone);
-  Message ralldone = receive(betaSocket);
-  send(alphaSocket, goodbye);
-  Message rgoodbye = receive(betaSocket);
+  auto routerFuture = std::async(std::launch::async,
+    [](bool& running)
+    {
+      SocketRouter router("testRouter", {});
+      router.poll(100, running);
+    }, std::ref(running));
 
   running = false;
-  ASSERT_EQ(hello, rhello);
-  ASSERT_EQ(heartbeat, rheartbeat);
-  ASSERT_EQ(problemspec, rproblemspec);
-  ASSERT_EQ(jobrequest, rjobrequest);
-  ASSERT_EQ(job, rjob);
-  ASSERT_EQ(jobswap, rjobswap);
-  ASSERT_EQ(alldone, ralldone);
-  ASSERT_EQ(goodbye, rgoodbye);
+  routerFuture.wait();
+}
+
+TEST(Router, routerPollCallsOnPollCallback)
+{
+  zmq::context_t context{1};
+  bool running = true;
+
+  Socket alpha{context, ZMQ_PAIR, "alpha"};
+  alpha.bind("inproc://alpha");
+
+  Socket beta{context, ZMQ_PAIR, "beta"};
+  beta.connect("inproc://alpha");
+
+  auto routerFuture = std::async(std::launch::async,
+    [](bool& running, Socket& alpha, Socket& beta)
+    {
+      SocketRouter router("testRouter", { &alpha, &beta });
+      router.bindOnPoll([&]() { running = false; });
+      router.poll(100, running);
+    }, std::ref(running), std::ref(alpha), std::ref(beta));
+
+  routerFuture.wait();
+  EXPECT_FALSE(running);
+}
+
+TEST(Router, socketSendCallsBoundCallback)
+{
+  zmq::context_t context{1};
+  bool running = true;
+
+  Socket alpha{context, ZMQ_PAIR, "alpha"};
+  alpha.bind("inproc://alpha");
+
+  Socket beta{context, ZMQ_PAIR, "beta"};
+  beta.connect("inproc://alpha");
+
+  auto routerFuture = std::async(std::launch::async,
+    [](bool& running, Socket& alpha, Socket& beta)
+    {
+      bool alphaReceived = false;
+      bool betaReceived = false;
+
+      SocketRouter router("testRouter", { &alpha, &beta });
+
+      router.bind(0, REQUEST, [&](const Message& m)
+      {
+        alphaReceived = true;
+        EXPECT_EQ(REQUEST, m.subject);
+        ASSERT_EQ(1U, m.data.size());
+        EXPECT_EQ("request", m.data[0]);
+      });
+
+      router.bind(1, JOB, [&](const Message& m)
+      {
+        betaReceived = true;
+        EXPECT_EQ(JOB, m.subject);
+        ASSERT_EQ(1U, m.data.size());
+        EXPECT_EQ("job", m.data[0]);
+      });
+
+      router.bindOnPoll([&]()
+      {
+        // Wait until both messages were received
+        if (alphaReceived && betaReceived)
+          running = false;
+      });
+
+      router.poll(100, running);
+
+      ASSERT_TRUE(alphaReceived);
+      ASSERT_TRUE(betaReceived);
+
+    }, std::ref(running), std::ref(alpha), std::ref(beta));
+
+  // Trigger the callbacks
+  beta.send({REQUEST, { "request" }});
+  alpha.send({JOB, { "job" }});
+
+  routerFuture.wait();
 }
