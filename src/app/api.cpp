f3ed11ca5976cd60313d22d9a1448874a64e0ec9
darrnshn <darren.shen@nicta.com.au>
2015-11-05 17:00:24 +1100

    Add backend server and frontend dashboard

--- src/app/api.cpp
+++ src/app/api.cpp
@@ -1,3 +1,5 @@
+#include "api.hpp"
+
 #include <server_http.hpp>
 #include <future>
 
@@ -5,16 +7,39 @@ using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
 
 namespace stateline
 {
+  void ApiResources::set(const std::string& name, json data)
+  {
+    mutex_.lock();
+    resources_[name] = data.dump();
+    mutex_.unlock();
+  }
+
+  std::string ApiResources::get(const std::string& name)
+  {
+    mutex_.lock();
+    std::string result = resources_[name];
+    mutex_.unlock();
+    return result;
+  }
+
   void runApiServerThread(HttpServer& server)
   {
     server.start();
   }
 
-  void runApiServer(uint port, bool& running)
+  void runApiServer(uint port, ApiResources& res, bool& running)
   {
     HttpServer server(port, 4);
+
     server.resource["^/test$"]["GET"] = [](HttpServer::Response &resp, std::shared_ptr<HttpServer::Request> request) {
       resp << "HTTP/1.1 200 OK\r\nContent-Length: " << 5 << "\r\n\r\n" << "hello";
+    };
+
+    server.resource["^/chains$"]["GET"] =
+      [&res](HttpServer::Response &resp, std::shared_ptr<HttpServer::Request> request) {
+
+      std::string content = res.get("chains");
+      resp << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
     };
 
     auto future = std::async(std::launch::async, runApiServerThread, std::ref(server));
--- src/app/api.hpp
+++ src/app/api.hpp
@@ -1,6 +1,22 @@
 #pragma once
 
+#include <json.hpp>
+#include <mutex>
+
+using json = nlohmann::json;
+
 namespace stateline
 {
-  void runApiServer(uint port, bool& running);
+  class ApiResources {
+    public:
+      std::string get(const std::string& resource);
+      void set(const std::string& resource, json data);
+
+      friend void runApiServer(uint, bool&);
+    private:
+      std::map<std::string, std::string> resources_;
+      std::mutex mutex_;
+  };
+
+  void runApiServer(uint port, ApiResources& res, bool& running);
 }
