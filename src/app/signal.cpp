48146010b4c17a9c95dcfed8891da6de45a64574
Hamish Ivey-Law <hamish.ivey-law@nicta.com.au>
2016-03-22 21:06:24 +1100

    Fix indentation offset.

--- src/app/signal.cpp
+++ src/app/signal.cpp
@@ -20,26 +20,26 @@ std::atomic_bool stateline::global::interruptedBySignal = ATOMIC_VAR_INIT(false)
 
 void stateline::init::handleSignal(int sig)
 {
-    VLOG(1) << "Caught Signal of type " << sig;
-    stateline::global::interruptedBySignal = true;
+  VLOG(1) << "Caught Signal of type " << sig;
+  stateline::global::interruptedBySignal = true;
 }
 
 void stateline::init::threadHandle()
 {
-    std::signal(SIGINT, handleSignal);
-    std::signal(SIGTERM, handleSignal);
-    while(!global::interruptedBySignal)
-    {
-        std::this_thread::sleep_for(std::chrono::seconds(60));
-    }
+  std::signal(SIGINT, handleSignal);
+  std::signal(SIGTERM, handleSignal);
+  while(!global::interruptedBySignal)
+  {
+    std::this_thread::sleep_for(std::chrono::seconds(60));
+  }
 }
 
 void stateline::init::initialiseSignalHandler()
 {
-    // Install a signal handler
-    // std::signal(SIGINT, handleSignal);
-    // std::signal(SIGTERM, handleSignal);
-    std::signal(SIGINT, SIG_IGN);
-    std::signal(SIGTERM, SIG_IGN);
-    std::thread(threadHandle).detach();
+  // Install a signal handler
+  // std::signal(SIGINT, handleSignal);
+  // std::signal(SIGTERM, handleSignal);
+  std::signal(SIGINT, SIG_IGN);
+  std::signal(SIGTERM, SIG_IGN);
+  std::thread(threadHandle).detach();
 }
