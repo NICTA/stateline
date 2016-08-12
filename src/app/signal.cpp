22575e57d8936391522b83abee0e84ef5dc7fd54
Darren Shen <darren.shen@data61.csiro.au>
2016-08-12 13:17:48 +1000

    Deprecate signals

--- src/app/signal.cpp
+++ src/app/signal.cpp
@@ -8,6 +8,11 @@
 //! \copyright (c) 2014, NICTA
 //!
 
+// DO NOT USE
+// C++ std::signal causes undefined behaviour in multithreaded programs
+// http://en.cppreference.com/w/cpp/utility/program/signal
+
+/*
 #include <csignal>
 #include <easylogging/easylogging++.h>
 #include <chrono>
@@ -18,28 +23,26 @@
 
 std::atomic_bool stateline::global::interruptedBySignal = ATOMIC_VAR_INIT(false);
 
-void stateline::init::handleSignal(int sig)
+void handleSignal(int sig)
 {
-  VLOG(1) << "Caught Signal of type " << sig;
+  SL_LOG(INFO) << "Caught Signal of type " << sig;
   stateline::global::interruptedBySignal = true;
 }
 
-void stateline::init::threadHandle()
-{
-  std::signal(SIGINT, handleSignal);
-  std::signal(SIGTERM, handleSignal);
-  while(!global::interruptedBySignal)
-  {
-    std::this_thread::sleep_for(std::chrono::seconds(60));
-  }
-}
-
-void stateline::init::initialiseSignalHandler()
+void initialiseSignalHandler()
 {
   // Install a signal handler
-  // std::signal(SIGINT, handleSignal);
-  // std::signal(SIGTERM, handleSignal);
-  std::signal(SIGINT, SIG_IGN);
-  std::signal(SIGTERM, SIG_IGN);
-  std::thread(threadHandle).detach();
+  std::signal(SIGINT, handleSignal);
+  std::signal(SIGTERM, handleSignal);
+  std::signal(SIGPROF, handleSignal);
+  //std::signal(SIGINT, SIG_IGN);
+  //std::signal(SIGTERM, SIG_IGN);
+  //std::thread(threadHandle).detach();
 }
+
+bool isRunning()
+{
+  return !interruptedBySignal.get();
+}
+
+}*/
